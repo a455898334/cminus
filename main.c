33a277f8ebece169958688832a1c887c636b86b9
whantae jiwhantae ji <jht008@gmail.com>
2014-10-02 23:02:15 +0900

    set EchoSource and TraceScan

--- main.c
+++ main.c
@@ -37,8 +37,8 @@ FILE * listing;
 FILE * code;
 
 /* allocate and set tracing flags */
-int EchoSource = FALSE;
-int TraceScan = FALSE;
+int EchoSource = TRUE;
+int TraceScan = TRUE;
 int TraceParse = FALSE;
 int TraceAnalyze = FALSE;
 int TraceCode = FALSE;
